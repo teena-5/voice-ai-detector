3f453d44fa8a9560060d82542ce90e40a37f2a25
K Tanuja <160623740019@stanley.edu.in>
2026-02-03 18:45:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ from utils import save_base64_audio
 from feature_extractor import extract_features
 from model import predict
 
-app = FastAPI(title="AI Voice Detection API")
+app = FastAPI(title="AI Voice Detection API", root_path="")
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
 
 
 @app.get("/")
@@ -19,6 +22,7 @@ class RequestBody(BaseModel):
     audio_base64: str
     language: str
     api_key: str
+    
 @app.post("/detect-voice")
 def detect_voice(data: RequestBody):
 
@@ -49,6 +53,7 @@ def detect_voice(data: RequestBody):
         "processing_time_ms": int((time.time() - start) * 1000)
     }
     
+
 
 
 
